9e9fed49deb056fd59f882c6471a9c8136139f67
Boutiti Nizar <boutitinizar@gmail.com>
2017-10-31 18:07:55 +0100

    SubPages

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -12,6 +12,8 @@ namespace Inc\Api;
 class SettingsApi
 {
     public  $admin_pages = [];
+    public  $admin_subpages = [];
+
     public function register()
     {
         if(!empty($this->admin_pages)){
@@ -24,11 +26,43 @@ class SettingsApi
         return $this;
     }
 
+    public function withSubPage(string $title = null){
+        if(empty($this->admin_pages)){
+           return $this;
+        }
+        $admin_page = $this->admin_pages[0];
+
+        $subpages = [
+            [
+                'parent_slug'=>$admin_page['menu_slug'],
+                'page_title'=>$admin_page['page_title'],
+                'menu_title'=>($title)? $title: $admin_page['menu_title'],
+                'capability'=>$admin_page['capability'],
+                'menu_slug'=>$admin_page['menu_slug'],
+                'callback'=>function(){echo'<h1>Plugin</h1>';}
+            ]
+        ];
+
+        $this->admin_subpages = $subpages;
+        return $this;
+    }
+
+    public function addSubPages(array $pages)
+    {
+        $this->admin_subpages = array_merge($this->admin_subpages,$pages);
+        return $this;
+    }
+
     public function  addAdminMenu()
     {
         foreach($this->admin_pages as $page){
             #code
             add_menu_page($page['page_title'],$page['menu_title'],$page['capability'],$page['menu_slug'],$page['callback'],$page['icon_url'],$page['position']);
         }
+        foreach($this->admin_subpages as $page){
+            #code
+            add_submenu_page($page['parent_slug'],$page['page_title'],$page['menu_title'],$page['capability'],$page['menu_slug'],$page['callback']);
+        }
     }
+
 }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -16,6 +16,7 @@ class Admin extends BaseController
 {
     public $settings;
     public $pages = [];
+    public $subpages = [];
     public function __construct()
     {
         $this->settings = new SettingsApi();
@@ -25,17 +26,26 @@ class Admin extends BaseController
                 'menu_title'=>'Milestone',
                 'capability'=>'manage_options',
                 'menu_slug'=>'milestone_plugin',
-                'callback'=>function(){echo'<h1>Plugin</h1>';},
+                'callback'=>function(){echo'<h1>Milestone Plugin</h1>';},
                 'icon_url'=>'dashicons-admin-multisite',
                 'position'=>'2',
             ]
         ];
-
+        $this->subpages = [
+            [
+                'parent_slug'=>'milestone_plugin',
+                'page_title'=>'Custome Post Types',
+                'menu_title'=>'CPT',
+                'capability'=>'manage_options',
+                'menu_slug'=>'milestone_cpt',
+                'callback'=>function(){echo'<h1>Custome Post types Mager</h1>';},
+            ]
+        ];
     }
 
     public function register()
     {
-        $this->settings->addPages($this->pages)->register();
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
 
 
